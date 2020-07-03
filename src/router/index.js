4675eda3641a86229e321af1c75cf5caf8434c60
刘志勇 <liuzhiyong@whhcxw.cn>
2020-07-03 17:17:08 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Util from "../plugins/util"
 
 Vue.use(VueRouter)
 
@@ -13,67 +12,12 @@ VueRouter.prototype.push = function push(location) {
 const routes = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/front'
   },
   {
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/Login.vue'),
-    meta: {
-      title: "登录",
-      roles: ["*"]
-    }
-  },
-  {
-    path: '/home',
-    name: 'home',
-    component: () => import('../views/Home.vue'),
-    meta: {
-      title: "首页",
-      roles: ["*"]
-    },
-    children: [
-      {
-        path: '/',
-        name: 'Welcome',
-        component: () => import('../views/Welcome.vue'),
-        meta: {
-          title: "欢迎",
-          roles: ["*"]
-        }
-      },
-      {
-        path: '/location',
-        name: 'Location',
-        component: () => import('../views/Location/Location.vue'),
-        meta: {
-          title: "位置管理",
-          roles: ["*"]
-        }
-      },
-      {
-        path: '/404',
-        name: '404',
-        component: () => import('../views/404.vue'),
-        meta: {
-          title: "404",
-          roles: ["*"]
-        }
-      },
-      {
-        path: '*',
-        name: '404',
-        component: () => import('../views/404.vue'),
-        meta: {
-          title: "404",
-          roles: ["*"]
-        }
-      }
-    ]
-  },
-  {
-    path: '*',
-    redirect: '/404'
+    path: '/front',
+    name: 'front',
+    component: () => import('../views/Front.vue')
   }
 ]
 
@@ -83,32 +27,5 @@ const router = new VueRouter({
   routes
 })
 
-router.beforeEach((to, from, next) => {
-  if (to.meta.title) {
-    document.title = to.meta.title;
-  }
-  const JwtToken = sessionStorage.getItem('JwtToken');
-  if (!JwtToken && to.path !== '/login') {
-    next('/login');
-  }
-  else if (to.path == '/404') {
-    next();
-  }
-  else {
-    // 权限管理
-    if (to.meta.roles.indexOf("*") >= 0) {
-      next();
-    }
-    else if (to.meta.roles.indexOf(JSON.parse(
-      Util.uncompileStr(sessionStorage.getItem("UserInfo"))
-    ).role) >= 0) {
-      next();
-    }
-    else {
-      next("/404");
-    }
-    // next();
-  }
-})
 
 export default router
---
+++ src/views/Front.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>1</div>
+</template>
+
+<script>
+export default {
+  name: "front",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+<style lang="scss">
+</style>
